9631fd23406565be6325ea8f32697e1524f55e8f
Avery Follett <avery.follett@mymail.champlain.edu>
2020-05-01 18:32:44 -0400

    Added GameTimer script

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    public float timerStartingTime;
+    private float timeRemaining;
+    public bool isTimerRunning;
+
+    private void Start()
+    {
+        timeRemaining = timerStartingTime;
+        StartTimer();
+    }
+
+    public void StartTimer()
+    {
+        isTimerRunning = true;
+    }
+
+    public void StopTimer()
+    {
+        isTimerRunning = false;
+    }
+
+    public void ResetTimer()
+    {
+        timeRemaining = timerStartingTime;
+    }
+
+    private void Update()
+    {
+        if (isTimerRunning)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+    }
+}
