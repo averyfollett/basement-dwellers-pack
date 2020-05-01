3aba013cdd48eed5e5f510bbad3a1a7c8b1f2d40
Avery Follett <avery.follett@mymail.champlain.edu>
2020-05-01 12:15:01 -0400

    Made placeholder player + camera and movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(-10, 0, 0));
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+
+        }
+    }
+}
