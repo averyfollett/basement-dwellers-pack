ed23d42e49fceb0e109b9840f3e1a3fcfe7af29e
goaliegamer1 <nicknackluckydog@gmail.com>
2020-05-01 19:38:09 -0400

    game manager spawn boxes at specific intervals

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject boxPrefab;
+    public GameObject startConv;
+    public int numBoxes;
+    public int interval;
+    public bool sendingBoxes;
+    float curTime;
+    GameTimer timer;
+    int prevTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sendingBoxes = true;
+        timer = this.GetComponent<GameTimer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sendingBoxes && numBoxes > 0)
+        {
+            if(Mathf.FloorToInt(timer.GetTimeRemaining()) % interval == 0 && prevTime != Mathf.FloorToInt(timer.GetTimeRemaining()))
+            {
+                prevTime = Mathf.FloorToInt(timer.GetTimeRemaining());
+                GameObject box = Instantiate(boxPrefab);
+                startConv.GetComponent<Conveyer>().box = box;
+                numBoxes--;
+            }
+        }
+    }
+}
