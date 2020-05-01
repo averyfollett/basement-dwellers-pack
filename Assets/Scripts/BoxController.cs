9cc94da78b134b1fd864ece5df90eca6fdc08935
Avery Follett <avery.follett@mymail.champlain.edu>
2020-05-01 19:33:38 -0400

    Added two scripts to the box prefab for order generation

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public int maxBoxCapacity;
+    public int currentBoxCapacity;
+}
