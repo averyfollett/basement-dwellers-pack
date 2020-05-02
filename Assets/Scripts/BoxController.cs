dc4ac0cc689dcab5ab61b22682e9cea792025545
Avery Follett <avery.follett@mymail.champlain.edu>
2020-05-01 20:44:24 -0400

    Set up BoxController to change the mesh based on box capacity

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -6,4 +6,26 @@ public class BoxController : MonoBehaviour
 {
     public int maxBoxCapacity;
     public int currentBoxCapacity;
+    public Mesh smallBoxMesh;
+    public Mesh mediumBoxMesh;
+    public Mesh largeBoxMesh;
+
+    private void Start()
+    {
+        if (maxBoxCapacity <= 5)
+        {
+            // Small Box
+            GetComponent<MeshFilter>().mesh = smallBoxMesh;
+        }
+        else if (maxBoxCapacity <= 10)
+        {
+            // Medium Box
+            GetComponent<MeshFilter>().mesh = mediumBoxMesh;
+        }
+        else
+        {
+            // Large Box
+            GetComponent<MeshFilter>().mesh = largeBoxMesh;
+        }
+    }
 }
