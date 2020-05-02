18a3d142573d094ba6604c9682e504fbe7cde8b4
Avery Follett <avery.follett@mymail.champlain.edu>
2020-05-01 23:23:00 -0400

    New player movement mostly working, just smooth rotation broken

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Animator anim;
+    public float movementSpeed;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
         if (Input.GetButton("Horizontal"))
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Horizontal")));
+            GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Horizontal") * movementSpeed, 0, -Input.GetAxis("Horizontal") * movementSpeed));
+            anim.SetBool("isMoving", true);
         }
 
         if (Input.GetButton("Vertical"))
         {
-            GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Vertical")));
+            GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Vertical") * movementSpeed, 0, Input.GetAxis("Vertical") * movementSpeed));
+            anim.SetBool("isMoving", true);
         }
+
+        if (!Input.GetButton("Horizontal") && !Input.GetButton("Vertical"))
+        {
+            anim.SetBool("isMoving", false);
+        }
+
+        Vector3 movement = new Vector3(-Input.GetAxisRaw("Horizontal"), 0.0f, -Input.GetAxisRaw("Vertical"));
+        if (movement != Vector3.zero)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.5F);
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
     }
 }
