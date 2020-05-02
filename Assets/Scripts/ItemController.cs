eafc49fe6d71571aa90c02ea18ce824fd4818a80
goaliegamer1 <nicknackluckydog@gmail.com>
2020-05-01 22:47:53 -0400

    items are now prefabs

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public int itemId;
+    public bool wasDropped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetItemId() { return itemId; }
+    public void SetItemId(int i) { itemId = i; }
+
+    public bool GetWasDropped() { return wasDropped; }
+    public void setWasDropped(bool b) { wasDropped = b; }
+}
