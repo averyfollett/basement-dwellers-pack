21a30ea0a6106bad207c29e9233cf5fcf3bd1543
Avery Follett <avery.follett@mymail.champlain.edu>
2020-05-02 16:37:37 -0400

    Small changes to level 1, added cameracontroller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 cameraOffset;
+    private Vector3 playerPos;
+
+    private void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        cameraOffset = playerPos - transform.position;
+    }
+
+    private void Update()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.position = playerPos - cameraOffset;
+    }
+}
