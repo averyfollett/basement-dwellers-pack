a2a562681ef440890fd37e371c416363e33e6a8f
Kevin-OShea <kevinbarretoshea@gmail.com>
2020-05-01 22:18:03 -0400

    placeholder

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -4,31 +4,59 @@ using UnityEngine;
 
 public class PickupItem : MonoBehaviour
 {	
-	public GameObject[] inventory = new GameObject[8];
+	public GameObject[] lookupTable = new GameObject[8];
+	public int[,] inventory = new int[8,10];
 	public bool trigger = false;
 	public Collider col;
 	public int count = 0;
+	
     void Update()
     {
         // if within and press e, add box to the top
+		// if (Input.GetMouseButtonDown(0) && trigger == true)
+        // {
+			// string tag = col.tag;
+			// print(tag);
+			// if(tag == "TestNumber") 
+			// {
+				// // keep the size of the [0][a] a in the first index of the 2d
+				// int count = inventory[0,0];
+				// if(count < inventory[0].Length)
+				// {
+					// inventory[0,count] = col.gameObject;
+					// count += 1;
+				// }
+				// inventory[0,0] = count;
+			// }
+        // } 
+		// if (Input.GetMouseButtonDown(1)) 
+		// {
+			// if(count > 0)
+			// {
+				// count = count - 1;
+				// Vector3 pos = transform.position;
+				// Instantiate(inventory[count], new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
+				// inventory[count] = null;
+			// }
+		// }
+		
 		if (Input.GetMouseButtonDown(0) && trigger == true)
         {
-			if(count < 8)
+			string tag = col.tag;
+			print(tag);
+			if(tag == "TestNumber") 
 			{
-				inventory[count] = col.gameObject;
-				count += 1;
+				// keep the size of the [0][a] a in the first index of the 2d
+				int count = inventory[0,0];
+				if(count < inventory.GetLength(1))
+				{
+					inventory[0,count] = 1;
+					count += 1;
+				}
+				inventory[0,0] = count;
 			}
+			Debug.Log(inventory[0,0]);
         } 
-		if (Input.GetMouseButtonDown(1)) 
-		{
-			if(count > 0)
-			{
-				count = count - 1;
-				Vector3 pos = transform.position;
-				Instantiate(inventory[count], new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
-				inventory[count] = null;
-			}
-		}
     }
 	
 	public void OnTriggerEnter(Collider other)
@@ -45,5 +73,5 @@ public class PickupItem : MonoBehaviour
 }
 
 // add 9 specific objects, with a specific amount total
-// count needs to be based on inventory size 
+// complete - count needs to be based on inventory size 
 // if it was picked off the shelf and dropped, when picked up, delete it off the ground
