ac0b36722dd108cea4dc4678ac4fd6cd8ffe5976
goaliegamer1 <nicknackluckydog@gmail.com>
2020-05-02 20:14:17 -0400

    hotbar works with scrollling

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -7,7 +7,9 @@ public class UiManager : MonoBehaviour
 {
     public Image[] items = new Image[9]; 
     public Text[] itemCount = new Text[9];
+    public Image selected;
     public PickupItem inv;
+    public Font f;
 
 
     // Start is called before the first frame update
@@ -19,6 +21,7 @@ public class UiManager : MonoBehaviour
         {
             itemCount[i].transform.position = items[i].transform.position + new Vector3(-63, -39f, 0);
             itemCount[i].fontSize = 20;
+            itemCount[i].font = f;
         }
     }
 
@@ -28,13 +31,14 @@ public class UiManager : MonoBehaviour
         
     }
 
-    public void updateInv()
+    public void updateInv(int s)
     {
         int[] invSize = inv.inventorySize;
         for(int i = 0; i < 9; i++)
         {
             itemCount[i].text = invSize[i].ToString();
         }
+        selected.transform.position = items[s].transform.position;
     }
 
 }
