7be8c1118442cb17a6e03e9182f02cd1124f6c12
goaliegamer1 <nicknackluckydog@gmail.com>
2020-05-02 20:27:24 -0400

    boxes close when order is complete

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -9,11 +9,15 @@ public class BoxController : MonoBehaviour
     public Mesh smallBoxMesh;
     public Mesh mediumBoxMesh;
     public Mesh largeBoxMesh;
+    
+    public Mesh smallBoxClosed;
+    public Mesh mediumBoxClosed;
+    public Mesh largeBoxClosed;
     public bool orderCompleted;
 
     private void Start()
     {
-        maxBoxCapacity = Random.Range(3, 15);
+        maxBoxCapacity = Random.Range(4, 15);
 
         transform.Rotate(new Vector3(0.0f, Random.Range(0, 360), 0.0f));
 
@@ -34,5 +38,31 @@ public class BoxController : MonoBehaviour
         }
 
         GetComponent<OrderSystem>().GenerateOrder();
+    }
+    private void Update()
+    {
+        checkComplete();
+    }
+
+    private void checkComplete()
+    {
+        if(orderCompleted)
+        {
+            if (maxBoxCapacity <= 5)
+        {
+            // Small Box
+            GetComponent<MeshFilter>().mesh = smallBoxClosed;
+        }
+        else if (maxBoxCapacity <= 10)
+        {
+            // Medium Box
+            GetComponent<MeshFilter>().mesh = mediumBoxClosed;
+        }
+        else
+        {
+            // Large Box
+            GetComponent<MeshFilter>().mesh = largeBoxClosed;
+        }
+        }
     }
 }
